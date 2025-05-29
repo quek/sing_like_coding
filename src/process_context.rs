39f083c2abb1e7b08aacc86c74c52978b22616a8
Tahara Yoshinori <read.eval.print@gmail.com>
2025-05-29 19:17:57 +0900

    .

--- src/process_context.rs
+++ src/process_context.rs
@@ -3,13 +3,7 @@ use std::{ops::Range, pin::Pin};
 use crate::event_list::{EventListInput, EventListOutput};
 
 pub struct ProcessContext {
-    #[allow(dead_code)]
-    pub bpm: f64,
-    #[allow(dead_code)]
-    pub sample_rate: f64,
     pub steady_time: i64,
-    #[allow(dead_code)]
-    pub lpb: u16,
     pub play_p: bool,
     pub play_position: Range<i64>,
     pub channels: usize,
@@ -23,10 +17,7 @@ pub struct ProcessContext {
 impl Default for ProcessContext {
     fn default() -> Self {
         Self {
-            bpm: 120.0,
-            sample_rate: 48000.0,
             steady_time: 0,
-            lpb: 4,
             play_p: false,
             play_position: 0..0,
             channels: 2,
