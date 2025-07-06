6107cac2ff5a8f9285422e869d79e6fce9583ec5
Tahara Yoshinori <read.eval.print@gmail.com>
2025-07-06 09:16:14 +0900

    bpm

--- sing_like_coding/src/eval.rs
+++ sing_like_coding/src/eval.rs
@@ -4,16 +4,19 @@ use crate::app_state::AppState;
 
 pub struct Eval {}
 
+#[derive(Debug)]
 enum Word<'a> {
     Word(&'a str),
-    Number(i64),
+    Integer(i64),
+    Float(f64),
 }
 
 impl<'a> std::fmt::Display for Word<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Word::Word(word) => word.fmt(f),
-            Word::Number(number) => number.fmt(f),
+            Word::Integer(value) => value.fmt(f),
+            Word::Float(value) => value.fmt(f),
         }
     }
 }
@@ -23,13 +26,20 @@ impl Eval {
         let mut stack = vec![];
         for word in buffer.split_whitespace() {
             if let Ok(num) = word.parse::<i64>() {
-                stack.push(Word::Number(num));
+                stack.push(Word::Integer(num));
+            } else if let Ok(num) = word.parse::<f64>() {
+                stack.push(Word::Float(num));
             } else {
                 stack.push(Word::Word(word));
             }
         }
         if let Some(Word::Word(word)) = stack.pop() {
             match word {
+                "bpm" => match stack.pop() {
+                    Some(Word::Integer(value)) => state.bpm_set(value as f64)?,
+                    Some(Word::Float(value)) => state.bpm_set(value)?,
+                    _ => (),
+                },
                 "call" | "c" => {
                     if let Some(word) = stack.pop() {
                         state.eval_call(word.to_string())?;
