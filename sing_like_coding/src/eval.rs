1d6c2b485fb69e40acb0a3520401a1cf519f5df9
Tahara Yoshinori <read.eval.print@gmail.com>
2025-07-06 09:21:30 +0900

    Word ない方がいいみたい

--- sing_like_coding/src/eval.rs
+++ sing_like_coding/src/eval.rs
@@ -1,53 +1,27 @@
-use anyhow::Result;
-
 use crate::app_state::AppState;
+
+use anyhow::Result;
 
 pub struct Eval {}
 
-#[derive(Debug)]
-enum Word<'a> {
-    Word(&'a str),
-    Integer(i64),
-    Float(f64),
-}
-
-impl<'a> std::fmt::Display for Word<'a> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Word::Word(word) => word.fmt(f),
-            Word::Integer(value) => value.fmt(f),
-            Word::Float(value) => value.fmt(f),
-        }
-    }
-}
-
 impl Eval {
     pub fn eval(buffer: &str, state: &mut AppState) -> Result<()> {
-        let mut stack = vec![];
-        for word in buffer.split_whitespace() {
-            if let Ok(num) = word.parse::<i64>() {
-                stack.push(Word::Integer(num));
-            } else if let Ok(num) = word.parse::<f64>() {
-                stack.push(Word::Float(num));
-            } else {
-                stack.push(Word::Word(word));
-            }
-        }
-        if let Some(Word::Word(word)) = stack.pop() {
+        let mut stack = buffer.split_whitespace().collect::<Vec<_>>();
+        if let Some(word) = stack.pop() {
             match word {
-                "bpm" => match stack.pop() {
-                    Some(Word::Integer(value)) => state.bpm_set(value as f64)?,
-                    Some(Word::Float(value)) => state.bpm_set(value)?,
-                    _ => (),
-                },
+                "bpm" => {
+                    if let Some(Ok(value)) = stack.pop().map(|x| x.parse::<f64>()) {
+                        state.bpm_set(value)?;
+                    }
+                }
                 "call" | "c" => {
-                    if let Some(word) = stack.pop() {
-                        state.eval_call(word.to_string())?;
+                    if let Some(label) = stack.pop() {
+                        state.eval_call(label.to_string())?;
                     }
                 }
                 "label" | "l" => {
-                    if let Some(word) = stack.pop() {
-                        state.eval_label(word.to_string())?;
+                    if let Some(label) = stack.pop() {
+                        state.eval_label(label.to_string())?;
                     }
                 }
                 "ret" | "r" => {
